62332f73146763cea502c6091535dad52a0d7c29
Jdjohar <Jdeep514@gmail.com>
2023-10-19 04:55:08 +0000

    we

---
+++ lesson3/lesson.js
@@ -0,0 +1,39 @@
+const heros = ["hero1", "hero2", "hero3"];
+// console.log(heros);
+
+
+
+
+// console.log(myobje.age);
+// console.log(myobje.email);
+
+const myFunctionName= function(){
+    console.log("Some text here")
+    }
+    // console.log(myFunctionName);
+
+
+
+    let channelName = "Career Engine";
+    let anotherName = channelName
+
+    anotherName = "JD Web Services"
+
+    // console.log(channelName);
+    // console.log(anotherName);
+
+    let userone = 
+    {
+        name:"jashan",
+        age: 27,
+        email: "jdeep@gmail.com"
+    }
+
+    let usertwo = userone;
+
+    usertwo.email = "jashan@jashan.com"
+
+
+    console.log(userone.email);
+    console.log(usertwo.email);
+
